57ede9ba84878102815f5f8921053e06ad7aacbf
samed <samedsgrtmc@gmail.com>
2020-03-17 02:51:01 +0300

    local branch first commit

--- src/gitDay4Package/GitDay4.java
+++ src/gitDay4Package/GitDay4.java
@@ -6,7 +6,7 @@ public class GitDay4 {
 	public static void main(String[] args) {
 		
 		System.out.println("local first commit master branch");
-		
+		System.out.println("local branch first commit");
 		
 		
 		
